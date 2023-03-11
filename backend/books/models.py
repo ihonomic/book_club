9b5a9c78ff8a8715933d113ca1c04f9c0643a3e2
Ihon <onosetaleoseghale@gmail.com>
2023-03-11 16:54:51 +0100

    @wip/created book models and templates

--- backend/books/models.py
+++ backend/books/models.py
@@ -9,7 +9,7 @@ class BookCategory(models.Model):
     date_modified = models.DateTimeField(auto_now=True)
 
     def __str__(self) -> str:
-        return str(self.category_name)
+        return str(self.id)
 
 
 class Book(models.Model):
@@ -18,7 +18,8 @@ class Book(models.Model):
     book_price = models.FloatField()
     book_category = models.ForeignKey(
         BookCategory, on_delete=models.CASCADE, related_name="book_category_instance")
-    is_book_available = models.BooleanField(default=False)
+    is_book_available = models.BooleanField(default=True)
+    book_quantity = models.IntegerField(default=0)
     book_image = models.ImageField(
         upload_to="book", default="https://banner2.cleanpng.com/20180329/vke/kisspng-python-high-level-programming-language-language-5abd4cc0d3dc94.7432282215223553928678.jpg")
 
--- backend/books/views.py
+++ backend/books/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.views import View
 
+from .models import BookCategory, Book
+
 # Two ways to write views. Class base views (CBV) and function base views (FBV)
 # 4 types of requests. GET, POST, PUT, DELETE, (CRUD Operation)
 
@@ -14,10 +16,38 @@ class LandingPageView(View):
 
 class CreateBookView(View):
     template_name = "books/create.html"
+    # Fetch book categories
+    categories = BookCategory.objects.all()
+    context = {
+        "categories": categories
+    }
 
     def get(self, request):
-        context = {}
-        return render(request, self.template_name, context)
+        return render(request, self.template_name, self.context)
 
     def post(self, request):
-        return render(request, self.template_name, {})
+        if request.method == 'POST':
+            title = request.POST["title"]
+            category_id = request.POST["category"]
+            description = request.POST["description"]
+            price = request.POST["price"]
+            photo = request.POST["photo"]
+            quantity = request.POST["quantity"]
+
+            # print(title, category_id, description, price, photo, quantity)
+            # get category instance
+            # print(request.FILES, "<==== DEBUG")
+
+            category_instance = BookCategory.objects.get(id=category_id)
+
+            Book.objects.create(
+                book_title=title,
+                book_description=description,
+                book_price=price,
+                book_category=category_instance,
+                book_image=photo,
+                is_book_available=True,
+                book_quantity=quantity
+            )
+
+        return render(request, self.template_name, self.context)
